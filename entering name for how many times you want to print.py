7ecce2584d994725d761273a753aeebff792915a
AronBobby12022006 <aronbobbydaniel2028@cs.sjcetpalai.ac.in>
2024-10-26 11:05:07 +0530

    Add files via upload

---
+++ entering name for how many times you want to print.py
@@ -0,0 +1,6 @@
+#AUTHOR:Aron Bobby Daniel
+#DATE:1-02-2024
+name=input("enter your name")
+number=int(input("enter the number of times you want to print your name"))
+result=name*number
+print(result)
