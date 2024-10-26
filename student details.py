c885d61ed0a98fb034e8660f9d7af4632a5995e9
AronBobby12022006 <aronbobbydaniel2028@cs.sjcetpalai.ac.in>
2024-10-26 10:57:28 +0530

    Add files via upload

---
+++ student details.py
@@ -0,0 +1,12 @@
+"""Author:Aron bobby Daniel
+    date:1-10-2024
+    to view student name,roll no and cgpa"""
+
+name=input("enter the student name")
+roll_no=int(input("enter your reg no"))
+roll_no=roll_no+1
+cgpa=float(input("enter your cgpa"))
+result=cgpa*10
+print("the name of the student is:",name)
+print("roll_no of the student:",roll_no)
+print("CGPA:",result,"%")
