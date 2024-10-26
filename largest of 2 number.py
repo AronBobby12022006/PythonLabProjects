4d594b3d8e76e40db074bb2c44593e97c578c602
AronBobby12022006 <aronbobbydaniel2028@cs.sjcetpalai.ac.in>
2024-10-26 11:09:23 +0530

    Add files via upload

---
+++ largest of 2 number.py
@@ -0,0 +1,14 @@
+"""
+Author:Aron Bobby Daniel
+Date:14-10-2024
+Python programme to determine the largest of 2 numbers
+"""
+
+num1=int(input("Enter first number:"))
+num2=int(input("Enter second number:"))
+if num1>num2:
+    print("First number is greater than Second number ")
+elif num2>num1:
+    print("Second number is greater than first number")
+else:
+    print("First number is equal to Second number")
