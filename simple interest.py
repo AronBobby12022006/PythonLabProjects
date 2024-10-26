187355ee5ed2e93146d8eec1e1472a23b96fefb3
AronBobby12022006 <aronbobbydaniel2028@cs.sjcetpalai.ac.in>
2024-10-26 11:14:09 +0530

    Add files via upload

---
+++ simple interest.py
@@ -0,0 +1,10 @@
+"""
+Author:Aron Bobby Daniel
+Date:14-10-2024
+Python programme to determine the simple interest
+"""
+p=int(input("Enter Principle:"))
+r=float(input("Enter rate:"))
+t=int(input("Enter Year:"))
+simple_interest=p*t*r
+print("the simple  interest of this plan is:",simple_interest)
