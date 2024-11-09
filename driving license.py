d854369d7828077dfac47441aff09fdbf70dbd01
Aronbobby28 <aronbobbydaniel2028@cs.sjcetpalai.ac.in>
2024-11-09 14:53:12 +0530

    Add files via upload

---
+++ driving license.py
@@ -0,0 +1,13 @@
+"""
+Author:Aron Bobby Daniel
+Date:09-11-2024
+Python program to check the eligibility of driving license
+
+"""
+age=int(input("Enter your Age:"))
+if age>=18:
+    print("You are eligible for driving license")
+else:
+    print("You are not eligible for driving license")
+
+
