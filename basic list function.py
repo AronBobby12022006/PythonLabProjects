d76887adc7a78addee28315b33ba9e204fee16eb
Aronbobby28 <aronbobbydaniel2028@cs.sjcetpalai.ac.in>
2024-11-12 15:06:42 +0530

    Add files via upload

---
+++ basic list function.py
@@ -0,0 +1,53 @@
+my_list=[1,2,3,4,5]
+print(my_list)
+my_friends=['Aron','Alvin','Joe']
+print(my_friends)
+salary=["Joe",79,990.90]
+print(salary)
+student=['Joe','S1',[30,29,29]]
+print(student)
+print(student[2][2])
+even_numbers=list(range(2,11,2))
+for even_num in even_numbers:
+    print(even_numbers)
+
+for friend in my_friends:
+    print(my_friends)
+    print("HAPPY NEW YEAR:"+friend)
+
+a=[1,2,3]
+b=[4,5,6]
+c=a+b
+print(c)
+print(a)
+print(b)
+
+binary=[9,0] #equality operator
+binary_sequence=binary*60
+print(binary_sequence)
+even=[4,5,6]
+mul12=[4,5,6]
+print(even==mul12)
+
+num=[9,0,5] #euality operator
+num1=[4,5,3]
+print(num==num1)
+
+prime=[2,4,5] #comparison operator
+even=[2,4,6]
+print(prime>even)
+
+even=[2,4,6,8] #in operator to check a element is present in a list or not
+composite=[4,6,8]
+print(2 in even)
+
+
+
+
+
+
+
+
+
+
+
