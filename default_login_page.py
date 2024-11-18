cbffecad9fd11db35377d911d7550c7aca532136
Aronbobby28 <aronbobbydaniel2028@cs.sjcetpalai.ac.in>
2024-11-18 21:23:47 +0530

    Add files via upload

---
+++ default_login_page.py
@@ -0,0 +1,12 @@
+"""
+Author:Aron Bobby Daniel
+Date:18-11-2024
+"""
+a="admin"
+b="Password@123"
+Username=input("Enter Username:")
+Password=input("Enter password:")
+if Username==a and Password==b:
+    print("Login was successful")
+else:
+    print("Login Failed,Try again")
