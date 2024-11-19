006680d3f8e1387e07875e4bb5b54b1117da7149
Aronbobby28 <aronbobbydaniel2028@cs.sjcetpalai.ac.in>
2024-11-19 15:54:20 +0530

    Add files via upload

---
+++ basic_tuple.py
@@ -0,0 +1,29 @@
+games=("cricket","f1","football","baseball")
+players=("hamilton","max","leclerc","sainz")
+print(games[1])
+print(games[-3])
+
+
+print(len(games))
+combined_tuple=games+players
+print("tHE Combined tuple=",combined_tuple)
+repeat_tuple=("abijith ikka",)*3
+print(repeat_tuple)
+
+
+colors=("red","yellow","green","blue","orange")
+print(colors[-2])
+
+person=("Alice",25,"Engineer")
+name,age,job=person
+print(name)
+print(age)
+print(job)
+
+
+inventory=[
+
+
+
+
+
