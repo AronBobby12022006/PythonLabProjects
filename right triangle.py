76ad65d0adb9ca0df4725a49d4a0a03358cf76b1
Aronbobby28 <aronbobbydaniel2028@cs.sjcetpalai.ac.in>
2024-11-30 10:16:11 +0530

    Add files via upload

---
+++ right triangle.py
@@ -0,0 +1,19 @@
+def is_right_triangle(side1,side2,side3):
+    sides=[side1,side2,side3]
+    sides.sort()
+    if sides[2]**2==sides[0]**2+sides[1]**2:
+        return True
+    return False
+
+
+side1=int(input("Enter side 1:"))
+side2=int(input("Enter side 2:"))
+side3=int(input("Enter side3 3:"))
+if is_right_triangle(side1,side2, side3):
+    print("Its a right triangle")
+else:
+    print("Its not a right triangle")
+
+
+
+
