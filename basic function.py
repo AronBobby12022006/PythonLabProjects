67a33c69a895ca95e865eb7fc8321df0e398ede4
Aronbobby28 <aronbobbydaniel2028@cs.sjcetpalai.ac.in>
2024-11-30 09:53:18 +0530

    Add files via upload

---
+++ basic function.py
@@ -0,0 +1,7 @@
+
+def adding_number():
+    num1=int(input("Enter First Number:"))
+    num2=int(input("Enter Second Number:"))
+    sum=num1+num2
+    print("The Sum of the numbers is",sum)
+adding_number()
